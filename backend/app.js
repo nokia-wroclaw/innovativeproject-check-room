d5919b7aae6b39cf3c5993147024963352f5a89e
pzmarzly <pawo2500@gmail.com>
2020-03-08 16:03:05 +0100

    remove cookie support

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
@@ -10,7 +9,6 @@ var app = express();
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
 
 app.use('/', indexRouter);
 
