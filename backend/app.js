1a49a2a97d7644d057c41718a79a3aacb8a256ab
pzmarzly <pawo2500@gmail.com>
2020-03-08 16:31:00 +0100

    copy and adjust ESLint and husky config

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,14 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
+const express = require( 'express' );
+const logger = require( 'morgan' );
 
-var indexRouter = require('./routes/index');
+const indexRouter = require( './routes/index' );
 
-var app = express();
+const app = express();
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use( logger( 'dev' ) );
+app.use( express.json() );
+app.use( express.urlencoded( { extended: false } ) );
 
-app.use('/', indexRouter);
+app.use( '/', indexRouter );
 
 module.exports = app;
