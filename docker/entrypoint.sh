298eb4b1f4b7cbbd21f646fb52262e7aa49febb7
pzmarzly <pawo2500@gmail.com>
2020-03-13 20:04:26 +0100

    use shorter method for setting default port

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,12 +5,7 @@ set -eux
 HERE=$(dirname "$0")
 
 # If we are running outside Heroku, let's set a sane $PORT.
-# We need to disabe exiting on unset variable for a moment.
-set +u
-if [ -z "$PORT" ]; then
-    export PORT=8000
-fi
-set -u
+export PORT=${MY_PORT:-8000}
 
 # We need to take the port from the environment (given by Heroku,
 # within 3000-60000 range) and save it to nginx config.
