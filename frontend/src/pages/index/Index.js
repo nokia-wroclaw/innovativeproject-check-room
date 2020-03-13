3bda815cf7c68c3b3c5515e0afb19b97f6f7fb4b
oleks12345 <oleks0709@gmail.com>
2020-03-13 16:26:55 +0100

    add Header to Index page

--- frontend/src/pages/index/Index.js
+++ frontend/src/pages/index/Index.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Header from '../../components/Header/Header';
 
 const Index = () => {
    return (
       <div>
-         <h1>Hello World</h1>
+         <Header text="Hello World" />
       </div>
    );
 };
