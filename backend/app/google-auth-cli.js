33344ead5b44fe6caa74218e17b5e090b51d8356
oleks12345 <oleks0709@gmail.com>
2020-03-20 01:43:42 +0100

    create google authentication

---
+++ backend/app/google-auth-cli.js
@@ -0,0 +1,39 @@
+const readline = require( 'readline' );
+const fs = require( 'fs' );
+const { google } = require( 'googleapis' );
+
+// If modifying these scopes, delete token.json.
+const SCOPES = [ 'https://www.googleapis.com/auth/calendar.readonly' ];
+
+// Load client secrets from a local file.
+const credentialsStr = fs.readFileSync( 'credentials.json' );
+const credentials = JSON.parse( credentialsStr );
+
+// eslint-disable-next-line camelcase
+const { client_secret, client_id, redirect_uris } = credentials.installed;
+const oAuth2Client = new google.auth.OAuth2(
+   client_id, client_secret, redirect_uris[0] );
+
+const authUrl = oAuth2Client.generateAuthUrl( {
+   access_type: 'offline',
+   scope: SCOPES,
+} );
+console.log( 'Authorize this app by visiting this url:', authUrl );
+
+const rl = readline.createInterface( {
+   input: process.stdin,
+   output: process.stdout,
+} );
+rl.question( 'Enter the code from that page here: ', ( code ) => {
+   rl.close();
+   oAuth2Client.getToken( code, ( err, token ) => {
+      if ( err ) {
+         console.error( 'Error retrieving access token', err );
+
+         return;
+      }
+
+      fs.writeFileSync( 'token.json', JSON.stringify( token ) );
+      console.log( 'Created token.json!' );
+   } );
+} );
---
+++ backend/app/google-auth.js
@@ -0,0 +1,23 @@
+const fs = require( 'fs' );
+const { google } = require( 'googleapis' );
+
+// Load client secrets from a local file.
+const credentialsStr = fs.readFileSync( 'credentials.json' );
+const credentials = JSON.parse( credentialsStr );
+const auth = authorize();
+
+/** Create an OAuth2 client with the given credentials. */
+function authorize( ) {
+   // eslint-disable-next-line camelcase
+   const { client_secret, client_id, redirect_uris } = credentials.installed;
+   const oAuth2Client = new google.auth.OAuth2(
+      client_id, client_secret, redirect_uris[0] );
+
+   const token = fs.readFileSync( 'token.json' );
+   oAuth2Client.setCredentials( JSON.parse( token ) );
+
+   return oAuth2Client;
+}
+
+module.exports = { auth };
+
