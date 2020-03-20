d7e4b3ee2643d90ba48cfa3fa6614aeeb73bd1d6
oleks12345 <oleks0709@gmail.com>
2020-03-20 01:44:11 +0100

    create calendar functions

---
+++ backend/app/calendar.js
@@ -0,0 +1,34 @@
+const { google } = require( 'googleapis' );
+const { auth } = require( './google-auth' );
+
+const calendar = google.calendar( { version: 'v3', auth } );
+
+async function listCalendars( ) {
+   return new Promise( ( resolve, reject ) => {
+      calendar.calendarList.list( {}, ( err, res ) => {
+         if ( err )
+            reject( err );
+         else
+            resolve( res.data.items );
+      } );
+   } );
+}
+
+async function listEvents( calendarId = 'primary' ) {
+   return new Promise( ( resolve, reject ) => {
+      calendar.events.list( {
+         calendarId,
+         timeMin: ( new Date() ).toISOString(),
+         maxResults: 10,
+         singleEvents: true,
+         orderBy: 'startTime',
+      }, ( err, res ) => {
+         if ( err )
+            reject( err );
+         else
+            resolve( res.data.items );
+      } );
+   } );
+}
+
+module.exports = { listEvents, listCalendars };
