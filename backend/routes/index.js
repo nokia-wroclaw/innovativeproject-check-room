43a1770712382c310b3feea56fba15a39839ec83
oleks12345 <oleks0709@gmail.com>
2020-03-20 01:43:21 +0100

    create endpoints

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,4 +1,5 @@
 const express = require( 'express' );
+const { listEvents, listCalendars } = require( '../app/calendar' );
 
 const router = express.Router();
 
@@ -6,6 +7,23 @@ router.get( '/config', function ( req, res, next ) {
    res.send( {
       appName: 'Check Room',
    } );
+} );
+
+router.get( '/calendars', async function ( req, res, next ) {
+   const calendars = await listCalendars();
+   res.send( calendars );
+} );
+
+router.get( '/events/:calendar', async function ( req, res, next ) {
+   const calendarRequested = req.params.calendar;
+
+   // Ensure the calendar exists.
+   const calendars = await listCalendars();
+   const exists = calendars.some( ( calendar ) => calendar.id === calendarRequested );
+   if ( !exists ) throw new Error( 'Calendar does not exist.' );
+
+   const events = await listEvents( calendarRequested );
+   res.send( events );
 } );
 
 module.exports = router;
