8576fb7e91b54c2fd57964fc122db1a7c09ba5d8
oleks12345 <oleks0709@gmail.com>
2020-03-23 23:30:56 +0100

    create theme
    create theme to unify styles

---
+++ frontend/src/assets/styles/theme.js
@@ -0,0 +1,30 @@
+export const theme = {
+   white: '#ffffff',
+   primary: '#6320EE',
+   secondary: '#067BC2',
+   text: '#343231',
+   link: '#20A4F3',
+   font: {
+      size: {
+         xs: '1.4rem',
+         s: '1.6rem',
+         m: '2.5rem',
+         l: '3.5rem',
+         xl: '5rem',
+      },
+      weight: {
+         light: 300,
+         regular: 400,
+         semiBold: 600,
+         bold: 800,
+      },
+   },
+   maxWidth: '1080px',
+   mdq: {
+      xs: '@media (min-width: 425px)',
+      sm: '@media (min-width: 640px)',
+      md: '@media (min-width: 768px)',
+      lg: '@media (min-width: 1024px)',
+      xl: '@media(min-width: 1440px)',
+   },
+};
