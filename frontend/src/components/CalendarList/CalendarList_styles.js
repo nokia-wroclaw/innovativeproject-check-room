4b5171bc3147f2161cea96bb469ba353f6d3a6f7
oleks12345 <oleks0709@gmail.com>
2020-03-24 14:38:41 +0100

    create and style calendarList

---
+++ frontend/src/components/CalendarList/CalendarList_styles.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const StyledCalendarList = styled.ul`
+  padding: 25px 10px 10px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  list-style: none
+`;
