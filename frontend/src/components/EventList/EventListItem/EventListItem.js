417d490ed74f53315f7d23e4d5b93caafd9242a1
oleks12345 <oleks0709@gmail.com>
2020-03-26 22:31:06 +0100

    create eventListItem

---
+++ frontend/src/components/EventList/EventListItem/EventListItem.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {
+   StyledEventListItem,
+   EventHeader,
+   EventLink,
+   EventDescription,
+   DateItem,
+} from './EventListItem_styles';
+
+const EventListItem = ( { eventData } ) => {
+   console.log( eventData );
+   const {
+      summary,
+      htmlLink,
+      start: { dateTime: startTime },
+      end: { dateTime: endTime },
+   } = eventData;
+   const startDate = new Date( startTime );
+   const endDate = new Date( endTime );
+
+   return (
+      <StyledEventListItem>
+         <EventLink href={ htmlLink }>
+            <EventHeader>{ summary }</EventHeader>
+            <EventDescription>
+               <DateItem>
+                  Day: { startDate.toLocaleDateString() } 
+               </DateItem>
+               <DateItem>
+                  From: { startDate.toLocaleTimeString() } 
+               </DateItem>
+               { startDate.toLocaleDateString() !==
+                  endDate.toLocaleDateString() && (
+                  <DateItem>End Day: { endDate.toLocaleDateString() }</DateItem> ) }
+               <DateItem>
+               To: { endDate.toLocaleTimeString() }
+               </DateItem>
+            </EventDescription>
+         </EventLink>
+      </StyledEventListItem>
+   );
+};
+
+EventListItem.propTypes = {
+   eventData: PropTypes.shape( {
+      htmlLink: PropTypes.string.isRequired,
+      summary: PropTypes.string.isRequired,
+      start: PropTypes.shape( {
+         dateTime: PropTypes.string.isRequired,
+      } ).isRequired,
+      end: PropTypes.shape( {
+         dateTime: PropTypes.string.isRequired,
+      } ).isRequired,
+      // description: PropTypes.string,
+   } ).isRequired,
+};
+
+export default EventListItem;
