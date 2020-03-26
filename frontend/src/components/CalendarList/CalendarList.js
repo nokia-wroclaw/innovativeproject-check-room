59a3f5cff9da75bbcc79d005cbe7840090380fd7
oleks12345 <oleks0709@gmail.com>
2020-03-26 22:40:37 +0100

    fix path

--- frontend/src/components/CalendarList/CalendarList.js
+++ frontend/src/components/CalendarList/CalendarList.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import CalendarListItem from '../CalendarListItem/CalendarListItem';
+import CalendarListItem from './CalendarListItem/CalendarListItem';
 import { StyledCalendarList } from './CalendarList_styles';
 
 const CalendarList = ( { calendarsData } ) => {
