4323d46ee2c3e1473a2b86594c7edc387d131728
oleks12345 <oleks0709@gmail.com>
2020-03-26 22:32:03 +0100

    add AbortControler to Calendars

--- frontend/src/pages/Calendars/Calendars.js
+++ frontend/src/pages/Calendars/Calendars.js
@@ -7,26 +7,29 @@ const Calendars = () => {
    const [ calendars, setCalendars ] = useState( [] );
    const [ isLoading, setIsLoading ] = useState( true );
    useEffect( () => {
-      fetch( `${constants.url.API_URL}calendars` )
-         .then( ( response ) => {
-            return response.json();
-         } )
+      const controller = new AbortController();
+      const { signal } = controller;
+      
+      fetch( `${constants.url.API_URL}calendars`,{ signal } )
+         .then( ( response ) => response.json() )
          .then( ( data ) => {
             const calendarList = data.filter(
-               ( calendar ) => calendar.summary.slice( 0, 5 ) === 'ROOM_'
+               ( calendar ) => calendar.summary.slice( 0, 5 ) === 'ROOM_',
             );
             setCalendars( calendarList );
             setIsLoading( false );
          } );
 
-      return () => {};
+      return () => {
+         controller.abort();
+      };
    }, [] );
 
    return (
       <PageTemplate>
          { isLoading ? (
             <h1 style={ { textAlign: 'center', padding: '45px 20px' } }>
-               Loading
+             Loading
             </h1>
          ) : (
             <CalendarList calendarsData={ calendars } />
