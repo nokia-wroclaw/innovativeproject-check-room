7965d40de68951e2295f4a84de88f360755dfdb7
oleks12345 <oleks0709@gmail.com>
2020-04-03 02:32:23 +0200

    create and style calendarDay

---
+++ frontend/src/components/EventList/CalendarDay/CalendarDay.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+import EventListItem from '../EventListItem/EventListItem';
+import {
+   Day,
+   DayBorder,
+   GridlineRow,
+   StyledCalendarDay,
+   GridlineHour,
+} from './CalendarDay_styles';
+import DayName from './DayName/DayName';
+
+const CalendarDay = ( { day, events } ) => {
+   const eventsArray = events.map( ( event ) => (
+      <EventListItem key={ event.id } eventData={ event } />
+   ) );
+
+   return (
+      <StyledCalendarDay>
+         <Day>
+            <DayName day={ day } />
+            <DayBorder />
+            { _.times( 24 * 2, ( i ) => (
+               <GridlineRow
+                  key={ i }
+                  style={ {
+                     gridRow: `${( i + 1 ) * 2} / ${( i + 1 ) * 2}`,
+                  } }
+               />
+            ) ) }
+            { _.times( 24, ( i ) => (
+               <GridlineHour
+                  key={ i }
+                  style={ {
+                     gridRow: `${i * 4 + 2} / ${i * 4 + 2}`,
+                  } }
+               >
+                  { i }:00
+               </GridlineHour>
+            ) ) }
+         </Day>
+         { /* { eventsArray } */ }
+      </StyledCalendarDay>
+   );
+};
+
+CalendarDay.propTypes = {
+   day: PropTypes.string.isRequired,
+   events: PropTypes.arrayOf( PropTypes.object ).isRequired,
+};
+
+export default CalendarDay;
---
+++ frontend/src/components/EventList/CalendarDay/CalendarDay_styles.js
@@ -0,0 +1,42 @@
+import styled from 'styled-components';
+
+export const StyledCalendarDay = styled.div`
+   width: 100%;
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+`;
+
+export const DayBorder = styled.div`
+   grid-column: 2/3;
+   grid-row: 2/-1;
+   border: 2px #dddddd solid;
+`;
+
+export const Day = styled.div`
+   display: grid;
+   width: 90%;
+   grid-template-columns: auto minmax(100px, 1fr);
+   ${( { theme } ) => theme.mdq.lg} {
+      /* grid-template-columns: 0 1fr; */
+      width: 100%;
+   }
+   grid-template-rows: auto repeat(96, 10px);
+`;
+
+export const GridlineHour = styled.div`
+   grid-column: 1/2;
+   padding: 3px 7px;
+   font-size: ${( { theme } ) => theme.font.size.xs};
+   text-align: right;
+`;
+
+export const GridlineRow = styled.div`
+   grid-column: 2/3;
+   border-top: 1px #dddddd;
+   height: 0px;
+   border-top-style: solid;
+   &:nth-child(2n) {
+      border-top-style: dashed;
+   }
+`;
