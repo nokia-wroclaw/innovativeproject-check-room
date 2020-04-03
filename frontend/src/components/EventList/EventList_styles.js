6a57de9f776fb34a9ac6c9d983ae81115863f76d
oleks12345 <oleks0709@gmail.com>
2020-04-03 02:45:24 +0200

    add md breakpoint

--- frontend/src/components/EventList/EventList_styles.js
+++ frontend/src/components/EventList/EventList_styles.js
@@ -4,6 +4,9 @@ export const StyledEventList = styled.div`
    margin: 0 25px;
    display: grid;
    grid-template-columns: 1fr;
+   ${( { theme } ) => theme.mdq.md} {
+      grid-template-columns: repeat(2, 1fr);
+   }
    ${( { theme } ) => theme.mdq.lg} {
       grid-template-columns: repeat(4, 1fr);
       grid-gap: 10px;
