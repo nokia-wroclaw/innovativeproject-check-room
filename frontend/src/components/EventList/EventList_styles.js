22467446da3285b8873b038b58e7c891be1e1d41
oleks12345 <oleks0709@gmail.com>
2020-04-16 12:26:45 +0200

    use styled-compoents/macro
    replace styled-components with macro version, so we can easier debug, by
    changing settings of styled components, without needing to eject configs

--- frontend/src/components/EventList/EventList_styles.js
+++ frontend/src/components/EventList/EventList_styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 export const StyledEventList = styled.div`
    margin: 0 25px;
--- frontend/src/components/RoomData/RoomData_styles.js
+++ frontend/src/components/RoomData/RoomData_styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 export const StyledRoomData = styled.div`
   max-width: 400px;
