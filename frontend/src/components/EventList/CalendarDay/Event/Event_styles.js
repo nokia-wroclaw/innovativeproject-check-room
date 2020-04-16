463139edfad785ddf6c4bd837ea6710d370da3bc
oleks12345 <oleks0709@gmail.com>
2020-04-16 13:09:12 +0200

    fix displaying long names

--- frontend/src/components/EventList/CalendarDay/Event/Event_styles.js
+++ frontend/src/components/EventList/CalendarDay/Event/Event_styles.js
@@ -3,18 +3,24 @@ import { rgba } from 'polished';
 
 export const StyledEvent = styled.div`
    display: flex;
-   justify-content: center;
    align-items: center;
    background: ${( { theme } ) => rgba( theme.primary, 0.4 )};
    overflow: hidden;
+   border-radius: 3px;
+   margin-left: 2px;
+   margin-right: 2px;
+   white-space: nowrap;
 `;
 
 export const EventName = styled.p`
+   display: block;
    margin: 0;
+   padding: 3px;
    text-align: center;
 `;
 
 export const EventLink = styled.a`
+   width: 100%;
    color: ${( { theme } ) => theme.text};
    text-decoration: none;
 `;
