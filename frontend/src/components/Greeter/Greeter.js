7ef75c812492f9a88a30590ee40fbf2b0dc164e7
pzmarzly <pawo2500@gmail.com>
2020-04-17 04:00:51 +0200

    add a simple home page

---
+++ frontend/src/components/Greeter/Greeter.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+import { CenteredBlock } from './Greeter_styles';
+
+const Index = ( { amountOfRooms } ) => {
+   return (
+      <CenteredBlock>
+         <h3>Welcome to Check Room.</h3>
+         <p>See <Link to="/calendars">Calendars</Link> for room availability information.</p>
+         <p>We are currently managing { amountOfRooms } rooms.</p>
+      </CenteredBlock>
+   );
+};
+
+Index.propTypes = {
+   amountOfRooms: PropTypes.oneOfType( [ PropTypes.string, PropTypes.number ] ).isRequired,
+};
+
+export default Index;
--- frontend/src/pages/index/Index.js
+++ frontend/src/pages/index/Index.js
@@ -1,11 +1,25 @@
-import React from 'react';
-import Header from '../../components/Header/Header';
+import React, { useState, useContext, useEffect } from 'react';
+import FetchContext from '../../services/fetching/FetchContext';
+import Greeter from '../../components/Greeter/Greeter';
 
 const Index = () => {
+   const [ amountOfRooms, setAmountOfRooms ] = useState( '(loading...)' );
+   const fetchAPI = useContext( FetchContext );
+
+   useEffect( () => {
+      const [ promise, abort ] = fetchAPI( 'calendars' );
+      promise.then( ( data ) => {
+         const calendarList = data.filter(
+            ( calendar ) => calendar.summary.slice( 0, 5 ) === 'ROOM_'
+         );
+         setAmountOfRooms( calendarList.length );
+      } );
+
+      return abort;
+   }, [ fetchAPI ] );
+
    return (
-      <div>
-         <Header text="Hello World" />
-      </div>
+      <Greeter amountOfRooms={ amountOfRooms } />
    );
 };
 
