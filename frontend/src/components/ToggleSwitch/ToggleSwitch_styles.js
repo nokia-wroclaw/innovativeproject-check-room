008872d06383a6ced7315b1dc578a90db7fe9d78
oleks12345 <oleks0709@gmail.com>
2020-04-24 02:24:55 +0200

    add color change

--- frontend/src/components/ToggleSwitch/ToggleSwitch_styles.js
+++ frontend/src/components/ToggleSwitch/ToggleSwitch_styles.js
@@ -4,14 +4,16 @@ import { lighten, darken } from 'polished';
 export const StyledToggleSwitch = styled.button`
    margin: 10px auto 20px;
    display: block;
-   background: ${( { theme, value } ) => ( value ? theme.secondary : 'none' )};
+   background: ${( { theme, value } ) =>
+      value ? theme.secondary : theme.primary};
+   color: ${( props ) => props.theme.white};
    border: solid grey 1px;
    font-size: ${( props ) => props.theme.font.size.s};
    padding: 10px 20px;
-   transition: transform 0.3s;
+   transition: transform 0.3s, background-color 0.3s;
    :hover {
       transform: scale(1.2);
       background: ${( { theme, value } ) =>
-      value ? darken( 0.1, theme.secondary ) : lighten( 0.1, theme.secondary )};
+      value ? darken( 0.1, theme.secondary ) : darken( 0.1, theme.primary )};
    }
 `;
