c3f2c4d640cb9023f6a55bca17a4d2dc5d39c994
pzmarzly <pawo2500@gmail.com>
2020-04-25 15:26:33 +0200

    remove Sentry backend monitoring

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,11 @@
 const express = require( 'express' );
 const logger = require( 'morgan' );
-const Sentry = require( '@sentry/node' );
 const cors = require( './auth/cors' );
 const tokenRequired = require( './auth/tokenRequired' );
 
 require( 'dotenv' ).config();
 
-Sentry.init( { dsn: process.env.SENTRY_DSN } );
-
 const app = express();
-
-app.use( Sentry.Handlers.requestHandler() );
 
 app.use( logger( 'dev' ) );
 app.use( express.json() );
@@ -25,7 +20,5 @@ app.use( tokenRequired() );
 const indexRouter = require( './routes/index' );
 
 app.use( '/api', indexRouter );
-
-app.use( Sentry.Handlers.errorHandler() );
 
 module.exports = app;
