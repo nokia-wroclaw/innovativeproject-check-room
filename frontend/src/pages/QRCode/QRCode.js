2fb3668eb993a1e430f711ba58ea46381be250fd
Aleksander Uniatowicz <oleks0709@gmail.com>
2020-04-30 13:42:08 +0200

    add qrCode page

---
+++ frontend/src/pages/QRCode/QRCode.js
@@ -0,0 +1,47 @@
+import React, { useEffect, useState, useContext, useReducer } from 'react';
+import QRCodeLib from 'qrcode.react';
+import { Link, useLocation , useParams } from 'react-router-dom';
+import moment from 'moment';
+import BackendContext from '../../services/communication/BackendContext';
+import { StyledQRCode } from './QRCode_styles';
+import RoomHeader from '../../components/RoomHeader/RoomHeader';
+
+const QRCode = () => {
+   const currentLink = window.location.href;
+   const link = currentLink.substring( 0, currentLink.lastIndexOf( '/' ) );
+   const path = useLocation().pathname;
+   const backLink = path.substring( 0, path.lastIndexOf( '/' ) );
+
+   const [ room, setRoom ] = useState( [] );
+   const [ isLoading, setIsLoading ] = useState( true );
+   const { roomId } = useParams();
+   const backend = useContext( BackendContext );
+
+   useEffect( () => {
+      const startDateTmp = moment()
+         .startOf( 'day' )
+         .toISOString();
+      const [ promise, abort ] = backend.fetchCalendar( roomId, startDateTmp );
+      promise.then( ( data ) => {
+         setRoom( data );
+         setIsLoading( false );
+      } );
+
+      return abort;
+   }, [ roomId, backend ] );
+
+   return (
+      <StyledQRCode>
+         { isLoading ? (
+            <h1 style={ { textAlign: 'center', padding: '45px 20px' } }>
+               Loading
+            </h1>
+         ) :
+            <RoomHeader roomData={ room.calendar } /> }
+         <Link to={ backLink }>Go Back</Link>
+         <QRCodeLib value={ link } size={ 200 } />
+      </StyledQRCode>
+   );
+};
+
+export default QRCode;
---
+++ frontend/src/pages/QRCode/QRCode_styles.js
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const StyledQRCode = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  a{
+     padding: 10px 20px 20px;
+  }
+`;
