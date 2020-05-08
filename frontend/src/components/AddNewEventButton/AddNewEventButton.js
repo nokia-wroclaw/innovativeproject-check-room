764072ecbba8accfbf57ca52ed20a9a17bda5cd9
pzmarzly <pawo2500@gmail.com>
2020-05-08 15:35:40 +0200

    add missing propTypes

--- frontend/src/components/AddNewEventButton/AddNewEventButton.js
+++ frontend/src/components/AddNewEventButton/AddNewEventButton.js
@@ -1,15 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { GoPlus } from 'react-icons/go';
 import { StyledAddNewEventButton } from './AddNewEventButton_styles';
 
 const AddNewEventButton = ( { openDrawer } ) => {
    return (
-      <StyledAddNewEventButton onClick = { openDrawer } >
+      <StyledAddNewEventButton onClick={ openDrawer } >
          <abbr title="New Event">
             <GoPlus />
          </abbr>
       </StyledAddNewEventButton>
    );
+};
+
+AddNewEventButton.propTypes = {
+   openDrawer: PropTypes.func.isRequired,
 };
 
 export default AddNewEventButton;
