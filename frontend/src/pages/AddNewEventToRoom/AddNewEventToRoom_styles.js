2f7bac3a9f9421fa84ec7eba5d20e01e3e3baf4c
pzmarzly <pawo2500@gmail.com>
2020-05-08 14:10:30 +0200

    use styled-components/macro everywhere

--- frontend/src/pages/AddNewEventToRoom/AddNewEventToRoom_styles.js
+++ frontend/src/pages/AddNewEventToRoom/AddNewEventToRoom_styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 export const StyledAddNewEventToRoom = styled.main`
   display: flex;
--- frontend/src/pages/RoomDetails/RoomDetails_styles.js
+++ frontend/src/pages/RoomDetails/RoomDetails_styles.js
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled from 'styled-components/macro';
 
 export const FlexCenter = styled.div`
    display: flex;
