e1cfa6660061a18386553501a6f2e1b00118f799
pzmarzly <pawo2500@gmail.com>
2020-05-08 09:58:12 +0200

    make location be displayed in 2 lines

--- frontend/src/components/RoomData/RoomData.js
+++ frontend/src/components/RoomData/RoomData.js
@@ -4,7 +4,7 @@ import ordinal from 'ordinal';
 import { FaChalkboard, FaChair } from 'react-icons/fa';
 import { GiFilmProjector } from 'react-icons/gi';
 import RoomMetadataDTO from '../../services/parsing/RoomMetadataDTO';
-import { StyledRoomData, RoomDescription, RoomLocation, RoomIndicators, Indicator } from './RoomData_styles';
+import { StyledRoomData, RoomDescription, RoomLocation, RoomIndicators, Indicator, LocationLine } from './RoomData_styles';
 
 const RoomData = ( { room } ) => {
    const { description, location, hasProjector, hasWhiteboard, seatsNo } = room;
@@ -12,7 +12,11 @@ const RoomData = ( { room } ) => {
    return (
       <StyledRoomData>
          { description ? <RoomDescription>{ description }</RoomDescription> : null }
-         { location ? <RoomLocation>{ location.building }, { ordinal( location.floorNo ) } floor</RoomLocation> : null }
+         { location ? (
+            <RoomLocation>
+               <LocationLine>{ location.building }, </LocationLine>
+               <LocationLine> { ordinal( location.floorNo ) } floor </LocationLine>
+            </RoomLocation> ) : null }
          <RoomIndicators>
             <Indicator active={ hasWhiteboard }><abbr title="Whiteboard"><FaChalkboard /></abbr></Indicator>
             <Indicator active={ hasProjector }><abbr title="Projector"><GiFilmProjector /></abbr></Indicator>
