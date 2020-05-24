a831552f10bf7871db3de79ded0c16efc21e8f0d
pzmarzly <pawo2500@gmail.com>
2020-05-24 17:35:43 +0200

    fix mongoose depreciation warning

--- backend/app/database/DBConnection.js
+++ backend/app/database/DBConnection.js
@@ -9,6 +9,7 @@ class DBConnection {
       try {
          await mongoose.connect( process.env.MONGO_URL, {
             useNewUrlParser: true,
+            useUnifiedTopology: true,
          } );
          DBConnection.connected = true;
       }
