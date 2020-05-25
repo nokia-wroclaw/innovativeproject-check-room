d81d252198cf8cbaeff88cb08651da2916b3858a
pzmarzly <pawo2500@gmail.com>
2020-05-26 00:27:39 +0200

    use antd loading indicator

---
+++ frontend/src/components/LoadingPage/LoadingPage_styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components/macro';
+
+export const StyledLoadingPage = styled.div`
+    width: 100%;
+    height: 100%;
+    min-height: 600px;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
