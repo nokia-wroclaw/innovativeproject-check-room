ab8aec8fc13fc6e38ec9a0ed7a9dd86148ea1673
pzmarzly <pawo2500@gmail.com>
2020-05-26 14:34:09 +0200

    decrease margins on event list page

--- frontend/src/components/RoomHeader/RoomHeader_styles.js
+++ frontend/src/components/RoomHeader/RoomHeader_styles.js
@@ -2,11 +2,11 @@ import styled from 'styled-components/macro';
 
 export const StyledRoomHeader = styled.div`
   max-width: 400px;
-  margin: 20px auto ;
+  margin: 20px auto 0 auto;
   text-align: center;
 `;
 
 export const RoomTitle = styled.h2`
    font-size: ${( { theme } )=>theme.font.size.l};
-   margin: 10px;
+   margin: 0;
 `;
