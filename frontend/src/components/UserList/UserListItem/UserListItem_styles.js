a7af6b0700b87c02ee1e52ad88759f9df819c926
Aleksander Uniatowicz <oleks0709@gmail.com>
2020-06-04 15:04:59 +0200

    make users panel responsive

--- frontend/src/components/UserList/UserListItem/UserListItem_styles.js
+++ frontend/src/components/UserList/UserListItem/UserListItem_styles.js
@@ -2,17 +2,24 @@ import styled from 'styled-components/macro';
 
 export const StyledUserListItem = styled.div`
    padding: 8px;
-
    display: grid;
    grid-gap: 2px;
-   grid-template-columns: 1fr 100px 70px;
+   grid-template-columns: minmax(0, 1fr) 100px 70px;
+   font-size: ${( { theme } ) => theme.font.size.xs};
+   ${( { theme } ) => theme.mdq.md} {
+      font-size: ${( { theme } ) => theme.font.size.s};
+   }
+   background: #ccc;
+   :nth-child(odd) {
+      background: #f5f5f5;
+   }
 `;
 
 export const UserInfo = styled.div`
+   word-wrap: break-word;
 `;
 
-export const UserType = styled.div`
-`;
+export const UserType = styled.div``;
 
 export const UserActions = styled.div`
    text-align: center;
