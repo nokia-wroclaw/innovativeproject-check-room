c0aeb8e49f3f48f1075abc640f68c90b3aed4cdb
pzmarzly <pawo2500@gmail.com>
2020-06-07 17:23:48 +0200

    style event removal dialog

--- frontend/src/components/EventList/CalendarDay/Event/EventDetailsModal/EventRemovalModal/EventRemovalModal.js
+++ frontend/src/components/EventList/CalendarDay/Event/EventDetailsModal/EventRemovalModal/EventRemovalModal.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Modal } from 'antd';
-import { DialogContent } from './EventRemovalModal_styles';
+import { DialogContent, EventTitle, EventDescription } from './EventRemovalModal_styles';
 
 const showEventRemovalModal = ( event, removeEvent ) => {
    Modal.confirm( {
       title: 'Are you sure you want to remove this event?',
       content: (
          <DialogContent>
-            <p>{ event.summary || '(no name)' }</p>
-            <p>{ event.description || 'No description.' }</p>
+            <EventTitle>{ event.summary || '(no name)' }</EventTitle>
+            <EventDescription>{ event.description || 'No description.' }</EventDescription>
          </DialogContent>
       ),
       maskClosable: true,
--- frontend/src/components/EventList/CalendarDay/Event/EventDetailsModal/EventRemovalModal/EventRemovalModal_styles.js
+++ frontend/src/components/EventList/CalendarDay/Event/EventDetailsModal/EventRemovalModal/EventRemovalModal_styles.js
@@ -3,3 +3,10 @@ import styled from 'styled-components/macro';
 export const DialogContent = styled.div`
    padding: 20px 0;
 `;
+
+export const EventTitle = styled.p`
+   text-align: center;
+`;
+
+export const EventDescription = styled.p`
+`;
