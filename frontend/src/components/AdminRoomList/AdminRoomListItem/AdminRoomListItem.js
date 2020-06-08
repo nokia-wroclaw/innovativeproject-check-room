41f1b56f34dafcd58ce3dc73cee58736cd0b4cb0
pzmarzly <pawo2500@gmail.com>
2020-06-08 17:49:52 +0200

    clear room cache on room removal

--- frontend/src/components/AdminRoomList/AdminRoomListItem/AdminRoomListItem.js
+++ frontend/src/components/AdminRoomList/AdminRoomListItem/AdminRoomListItem.js
@@ -19,6 +19,7 @@ const AdminRoomListItem = ( { room } ) => {
       promise.then( () => {
          setIsSaving( false );
          setIsDeleted( true );
+         backend.cache.reset();
       } );
    };
 
