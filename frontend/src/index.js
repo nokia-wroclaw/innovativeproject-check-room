82f15c3fcff9e2da440e8ef96cb93b0344891786
pzmarzly <pawo2500@gmail.com>
2020-06-15 20:08:53 +0200

    use latin ext version of Open Sans font

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import 'antd/dist/antd.css';
-import 'typeface-open-sans';
+import 'fontsource-open-sans/latin-ext.css';
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 
